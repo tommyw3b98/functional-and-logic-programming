34fc7e7aa8a0cc0820c898a2d6d2f2f05e5e4db3
tommy <tommy@ubuntu.home>
2022-03-29 10:08:18 +0200

    add local variables examples

--- source/haskell-examples/circonferenza.hs
+++ source/haskell-examples/circonferenza.hs
@@ -1,4 +1,6 @@
 {- Definizione della circonferenza dal basso verso l'alto -}
+{- L'ordine delle definizioni è irrilevante -}
+{- In questo caso le variabili create sono GLOBALI -}
 
 raggio :: Float 
 raggio = sqrt 15.196
@@ -8,3 +10,31 @@ diametro = 2 * raggio
 
 circonferenza :: Float
 circonferenza = pi * diametro
+
+{- Definizioni alternative -}
+
+
+{- Usando le definizioni seguenti possiamo creare variabili LOCALI alla 
+definizione di circonferenza (che non hanno quindi valore al di fuori di essa)-}
+
+{- Usando il costrutto LET-IN -}
+{-
+
+circonferenza :: Float
+circonferenza = let
+                    diametro = 2 * raggio
+                    raggio = sqrt 15.196
+                in
+                    pi * diametro
+
+-}
+
+{- Usando il costrutto WHERE -}
+{-
+
+circonferenza :: Float
+circonferenza = pi * diametro
+    where
+    	diametro = 2 * raggio
+    	raggio = sqrt 15.196
+-}
